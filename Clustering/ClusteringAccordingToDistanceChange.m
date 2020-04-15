93c71f8c5dacf9439bf10838effe76728693854d
Ori Ossmy <oriossmy@gmail.com>
2020-04-15 10:47:49 -0400

    Update clustering folder

---
+++ Clustering/ClusteringAccordingToDistanceChange.m
@@ -0,0 +1,35 @@
+function [dist_cl]=ClusteringAccordingToDistanceChange(data_file)
+% ClusteringAccordingToDistanceChange is the main script to group dyads
+% into clusters based on the change in distance between them throughout the
+% session
+%
+%% Syntax
+% ClusteringAccordingToDistanceChange(data_file)
+%
+%% Description
+% ClusteringAccordingToDistanceChange gets a data file with the changes
+% between partners, calculates the similarity between all dyads according
+% to the change in distance between the partners and performs density-peak
+% clustering to cluster the dyads to groups
+%
+% Required Input.
+% data_file: data_file with the changes in distance between the mom and
+% the baby.
+%
+% Output.
+% dist_cl: array with all the clusters according to the change in distance
+load(data_file)
+
+% calculates the percentage of distance change to use for similarity (how
+% much the mom and baby got closer and further and in which condition).
+% Data file should already include all the conditions according to the
+% order: distance closter-baby move | distance closer-mom move | distance
+% closer both move| distance further-baby move | distance further-mom move | distance
+% further both move| 
+subject_time_patterns = [mom_baby_dist_by_stats_segmants_closer./(sum(mom_baby_dist_by_stats_segmants_closer,2)) ...
+                         mom_baby_dist_by_stats_segmants_further./(sum(mom_baby_dist_by_stats_segmants_further,2))];
+
+clear mom_baby_dist_by_stats_getting_closer mom_baby_dist_by_stats_getting_further mom_baby_dist_by_stats_segmants_closer mom_baby_dist_by_stats_segmants_further
+
+% Cluster subject based on similarity between them
+[dist_cl]=cluster_subjects(pdist(subject_time_patterns),3);
--- Clustering/cluster_subjects.m
+++ Clustering/cluster_subjects.m
@@ -1,64 +1,53 @@
-%function [cl]=cluster_subjects(data,percent)
-data = subject_time_patterns;
-percent = 10
-mdist_square = squareform(pdist(data));
-mdist = [];
-for i=1:size(mdist_square,1)
-    for j=i:size(mdist_square,2)
-      mdist = [mdist; i j mdist_square(i,j)];  
-    end
-end
-%figure;
-%imagesc(mdist_square);
-% disp('The only input needed is a distance matrix file')
-% disp('The format of this file should be: ')
-% disp('Column 1: id of element i')
-% disp('Column 2: id of element j')
-% disp('Column 3: dist(i,j)')
-% mdist=input('name of the distance matrix file\n','s');
-% disp('Reading input distance matrix')
-%load('mom_baby_mdist');6
-xx=mdist;
-ND=max(xx(:,2));
-NL=max(xx(:,1));
+function [clusters_array]=cluster_subjects(mdist,density_cutoff)
+% cluster_subjects is an implementation of density-peak clustering. 
+%
+%% Syntax
+% [clusters_array]=cluster_subjects(mdist_square,percent)
+%
+%% Description
+% cluster_subjects gets a similiarity between each pair of samples and a cutoff distance 
+% and returns an array with the cluster number for each dyad. The
+% clustering is based on calculating the density between all samples
+% (according to the similarity matrix) and the distance between each
+% sample and the most densed sample that follows. The "percent" variable
+% provides a cutoff radius that can be calculated based on changes in
+% entropy.
+%
+% Required Input.
+% mdist_square: the similiarity values between each pair of samples 
+% percent: cut_off radius for calculating density
+%
+% Output.
+% clusters_array: includes the cluster number for each sample
+
+ND=max(mdist(:,2));
+NL=max(mdist(:,1));
 if (NL>ND)
   ND=NL;
 end
-N=size(xx,1);
+N=size(mdist,1);
 for i=1:ND
   for j=1:ND
     dist(i,j)=0;
   end
 end
 for i=1:N
-  ii=xx(i,1);
-  jj=xx(i,2);   
-  dist(ii,jj)=xx(i,3);
-  dist(jj,ii)=xx(i,3);
+  ii=mdist(i,1);
+  jj=mdist(i,2);   
+  dist(ii,jj)=mdist(i,3);
+  dist(jj,ii)=mdist(i,3);
 end
 
-position=round(N*percent/100);
-sda=sort(xx(:,3));
+% calculates the radius based on the density cut_off (in percentage)
+position=round(N*density_cutoff/100);
+sda=sort(mdist(:,3));
 dc=sda(position);
-
 fprintf('Computing Rho with gaussian kernel of radius: %12.6f\n', dc);
 
-
+%% Calculates density for each sample (rho array) and distance (delta array)
 for i=1:ND
   rho(i)=0.;
 end
-%
-% Gaussian kernel
-%
-% for i=1:ND-1
-%   for j=i+1:ND
-%      rho(i)=rho(i)+exp(-(dist(i,j)/dc)*(dist(i,j)/dc));
-%      rho(j)=rho(j)+exp(-(dist(i,j)/dc)*(dist(i,j)/dc));
-%   end
-% end
-%
-% "Cut off" kernel
-%
 for i=1:ND-1
  for j=i+1:ND
    if (dist(i,j)<dc)
@@ -67,13 +56,14 @@ for i=1:ND-1
    end
  end
 end
-
 maxd=max(max(dist));
 
+% sort density to calculate delta
 [rho_sorted,ordrho]=sort(rho,'descend');
 delta(ordrho(1))=-1.;
 nneigh(ordrho(1))=0;
 
+% builds delta array
 for ii=2:ND
    delta(ordrho(ii))=maxd;
    for jj=1:ii-1
@@ -88,6 +78,7 @@ disp('Generated file:DECISION GRAPH')
 disp('column 1:Density')
 disp('column 2:Delta')
 
+%% Choosing outlier version (gamma is calculated automatically in the next section)
 fid = fopen('DECISION_GRAPH', 'w');
 for i=1:ND
    fprintf(fid, '%6.2f %6.2f\n', rho(i),delta(i));
@@ -106,46 +97,50 @@ text(rho(:),delta(:),strread(num2str(ind)','%s'));
 title ('Decision Graph','FontSize',15.0)
 xlabel ('\rho')
 ylabel ('\delta')
-
-
 subplot(2,1,2)
 tt=plot(ones(length(ind),1),gamma(:),'o','MarkerSize',1,'MarkerFaceColor','k','MarkerEdgeColor','k');
 text(ones(length(ind),1),gamma(:),strread(num2str(ind)','%s'));
 title ('Decision Graph','FontSize',15.0)
 xlabel ('\rho')
 ylabel ('\delta')
-
-
-
-
-
 subplot(2,1,1)
 rect = getrect;
 rhomin=rect(1);
 deltamin=rect(2);
 NCLUST=0;
 for i=1:ND
-  cl(i)=-1;
+  clusters_array(i)=-1;
 end
 for i=1:ND
   if ( (rho(i)>rhomin) && (delta(i)>deltamin))
      NCLUST=NCLUST+1;
-     cl(i)=NCLUST;
+     clusters_array(i)=NCLUST;
      icl(NCLUST)=i;
   end
 end
 fprintf('NUMBER OF CLUSTERS: %i \n', NCLUST);
 disp('Performing assignation')
 
-%assignation
+%% calculating gamma automatically from standard deviation
+% gamma_outliers = find(gamma>(mean(gamma)+3*std(gamma)));
+% for i=1:ND
+%   if (ismember(i,gamma_outliers))
+%      NCLUST=NCLUST+1;
+%      clusters_array(i)=NCLUST;
+%      icl(NCLUST)=i;
+%   end
+% end
+
+%% assignation to clusters
 for i=1:ND
-  if (cl(ordrho(i))==-1)
-    cl(ordrho(i))=cl(nneigh(ordrho(i)));
+  if (clusters_array(ordrho(i))==-1)
+    clusters_array(ordrho(i))=clusters_array(nneigh(ordrho(i)));
   end
 end
-%halo
+
+%% Calculating halo around the clusters
 for i=1:ND
-  halo(i)=cl(i);
+  halo(i)=clusters_array(i);
 end
 if (NCLUST>1)
   for i=1:NCLUST
@@ -153,19 +148,19 @@ if (NCLUST>1)
   end
   for i=1:ND-1
     for j=i+1:ND
-      if ((cl(i)~=cl(j))&& (dist(i,j)<=dc))
+      if ((clusters_array(i)~=clusters_array(j))&& (dist(i,j)<=dc))
         rho_aver=(rho(i)+rho(j))/2.;
-        if (rho_aver>bord_rho(cl(i))) 
-          bord_rho(cl(i))=rho_aver;
+        if (rho_aver>bord_rho(clusters_array(i))) 
+          bord_rho(clusters_array(i))=rho_aver;
         end
-        if (rho_aver>bord_rho(cl(j))) 
-          bord_rho(cl(j))=rho_aver;
+        if (rho_aver>bord_rho(clusters_array(j))) 
+          bord_rho(clusters_array(j))=rho_aver;
         end
       end
     end
   end
   for i=1:ND
-    if (rho(i)<bord_rho(cl(i)))
+    if (rho(i)<bord_rho(clusters_array(i)))
       halo(i)=0;
     end
   end
@@ -174,7 +169,7 @@ for i=1:NCLUST
   nc=0;
   nh=0;
   for j=1:ND
-    if (cl(j)==i) 
+    if (clusters_array(j)==i) 
       nc=nc+1;
     end
     if (halo(j)==i) 
@@ -184,6 +179,7 @@ for i=1:NCLUST
   fprintf('CLUSTER: %i CENTER: %i ELEMENTS: %i CORE: %i HALO: %i \n', i,icl(i),nc,nh,nc-nh);
 end
 
+%% Print MDS space for the clusters
 cmap=colormap;
 for i=1:NCLUST
    ic=int8((i*64.)/(NCLUST*1.));
@@ -216,19 +212,11 @@ for i=1:NCLUST
   hold on
   plot(A(1:nn,1),A(1:nn,2),'o','MarkerSize',6,'MarkerFaceColor',cmap(ic,:),'MarkerEdgeColor',cmap(ic,:));
 end
-
-%for i=1:ND
-%   if (halo(i)>0)
-%      ic=int8((halo(i)*64.)/(NCLUST*1.));
-%      hold on
-%      plot(Y1(i,1),Y1(i,2),'o','MarkerSize',2,'MarkerFaceColor',cmap(ic,:),'MarkerEdgeColor',cmap(ic,:));
-%   end
-%end
 faa = fopen('CLUSTER_ASSIGNATION', 'w');
 disp('Generated file:CLUSTER_ASSIGNATION')
 disp('column 1:element id')
 disp('column 2:cluster assignation without halo control')
 disp('column 3:cluster assignation with halo control')
 for i=1:ND
-   fprintf(faa, '%i %i %i\n',i,cl(i),halo(i));
+   fprintf(faa, '%i %i %i\n',i,clusters_array(i),halo(i));
 end
